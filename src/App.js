4dcbf4e5ecd9ebde38af2e4c5d5612e61356f441
Augusto Lepe <augusto.lepe@mail.udp.cl>
2023-02-01 05:41:05 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BaseColaboradores } from "./basecolaboradores";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
+  const [nombre, setNombre] = useState("")
+  const [correo, setCorreo] = useState("")
+  const [buscar, setBuscar] = useState("")
+  const [colaboradores, setColaboradores] = useState(BaseColaboradores)
+
+  const agregar = (e) => {
+    e.preventDefault()
+    setColaboradores([...colaboradores, { id: colaboradores.length + 1, nombre, correo }])
+  }
+
+  const renderColaboradores = () => {
+    return colaboradores.filter((colaborador) => {
+      if (buscar === '') {
+        return colaborador
+      } else if (
+        colaborador.nombre.toLocaleLowerCase().includes(buscar.toLocaleLowerCase())
+      ) {
+        return colaborador
+      }
+    })
+      .map((colaborador) => (
+        <li key={colaborador.nombre}>
+          {colaborador.nombre} - {colaborador.correo}
+        </li>
+      ));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <nav class="navbar bg-dark bg-body-tertiary navbar-dark">
+          <div class="container-fluid">
+            <a class="navbar-brand">Buscador de colaboradores</a>
+            <form class="d-flex" role="search">
+              <input
+                class="form-control me-5"
+                type="search"
+                placeholder="Busca un colaborador"
+                onChange={(e) => setBuscar(e.target.value)} />
+            </form>
+          </div>
+        </nav>
+      </div>
+
+      <form class="ms-3 mt-5 w-75">
+        <label class="form-label">
+          Nombre del colaborador
+        </label>
+        <input
+          className="form-control"
+          name="nombreTarea"
+          placeholder="Ingresa nombre del colaborador"
+          onChange={(e) => setNombre(e.target.value)} />
+        <label class="form-label mt-3">
+          Correo del colaborador
+        </label>
+        <input
+          className="form-control"
+          name="nombreTarea"
+          placeholder="Ingresa email del colaborador"
+          onChange={(e) => setCorreo(e.target.value)} />
+        <button className="btn btn-primary mt-4 mb-3" onClick={agregar}>
+          Agregar colaborador
+        </button>
+        <hr></hr>
+        <h2>Listado de Colaboradores</h2>
+      </form>
+
+      <div>
+        <ul class="mt-3">{renderColaboradores()}</ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
